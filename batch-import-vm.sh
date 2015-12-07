4b50a8d9ff488b8e6e013c6fd9cfa77d66ed5fe2
garelp <pierre@garel.net>
2015-12-07 10:36:52 +0100

    added ovftool loging and verbosity

--- batch-import-vm.sh
+++ batch-import-vm.sh
@@ -44,7 +44,7 @@ do
 		    check_tmpl_exists $vcdTmpl
 		    if [ $? -eq 0 ]; then
 				set_vca_vdc "$vcaProfile" "$vcdPool"
-				ovftool --maxVirtualHardwareVersion=9 "$ovfPath" "vcloud://$vcdUser:$vcdPass@$vcdHost/?org=$vcdOrg&catalog=$vcdCatalog&vappTemplate=$vcdTmpl"
+				ovftool --maxVirtualHardwareVersion=9 --X:logFile=ovftool.log --X:logLevel=verbose "$ovfPath" "vcloud://$vcdUser:$vcdPass@$vcdHost/?org=$vcdOrg&catalog=$vcdCatalog&vappTemplate=$vcdTmpl"
 			else
 				echo "skipping $vcdTmpl already exists in catalog."
 			fi
