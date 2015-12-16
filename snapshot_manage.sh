1665cfa95bf2f64ba3f85525be95924561983306
garelp <pierre@garel.net>
2015-12-16 15:44:06 +0100

    update on snapshot_manage script

--- snapshot_manage.sh
+++ snapshot_manage.sh
@@ -31,11 +31,6 @@ if [ -z "$vcdUser" ]; then
 	exit 10
 fi
 
-if [ -z "$vappName" ]; then
-	echo "Please specify the Vapp Name."
-	exit 10
-fi
-
 if [ -z "$snapFunction" ]; then
 	echo "Please specify a snapshot function."
 	exit 10
@@ -54,30 +49,65 @@ source $libPath/vcloud-api-func
 
 case $snapFunction in
 	create)
+		if [ -z "$vappName" ]; then
+			echo "Please specify the Vapp Name."
+			exit 10
+		fi
 		create_vapp_snaphot $vappName
 	;;
 	remove)
+		if [ -z "$vappName" ]; then
+			echo "Please specify the Vapp Name."
+			exit 10
+		fi
 		remove_vapp_snapshot $vappName
 	;;
 	list)
-		snapInfo=$(get_vapp_snapshot $vappName)
-		snapDate=$( echo $snapInfo | cut -d , -f 3 )
-		if [ "$snapDate" ]
-		then
-			snapState=$( echo $snapInfo | cut -d , -f 2 )
-			snapSize=$( echo $snapInfo | cut -d , -f 1 )
-			if [ $snapState == "true" ]
-			then
-				snapState="PowerOn"
-			else
-				snapState="PowerOff"
-			fi
-			printf "| %-25s | %-20s | %-20s |\n" "Date taken" "vCD Snap Size (MB)" "Vapp Status" 
-			echo "|-------------------------------------------------------------------------|"
-			printf "| %-25s | %-20s | %-20s |\n" $snapDate $(($snapSize/1024/1024)) $snapState 
+		if [ -z "$vappName" ]; then
+			# No Vapp name so get all snapshot for current vDC
+			echo "-------------------------------------------------------------------------------------------------------"
+			printf "| %-25s | %-25s | %-20s | %-20s |\n" "Vapp Name" "Date taken" "vCD Snap Size (MB)" "Vapp Status" 
+			echo "|-----------------------------------------------------------------------------------------------------|"
+			for vapp in $(get_vapp_list)
+			do
+				snapInfo=$(get_vapp_snapshot $vapp)
+				snapDate=$( echo $snapInfo | cut -d , -f 3 )
+				if [ "$snapDate" ]
+				then
+					# A snapshot is present.
+					snapState=$( echo $snapInfo | cut -d , -f 2 )
+					snapSize=$( echo $snapInfo | cut -d , -f 1 )
+					if [ $snapState == "true" ]
+					then
+						snapState="PowerOn"
+					else
+						snapState="PowerOff"
+					fi
+					printf "| %-25s | %-25s | %-20s | %-20s |\n" $vapp $snapDate $(($snapSize/1024/1024)) $snapState 
+				fi				
+			done
+			echo "-------------------------------------------------------------------------------------------------------"
 		else
-			echo "No snapshot found for $vappName."
-			exit 1
+			# Display snapshot infor for the given VappName
+			snapInfo=$(get_vapp_snapshot $vappName)
+			snapDate=$( echo $snapInfo | cut -d , -f 3 )
+			if [ "$snapDate" ]
+			then
+				snapState=$( echo $snapInfo | cut -d , -f 2 )
+				snapSize=$( echo $snapInfo | cut -d , -f 1 )
+				if [ $snapState == "true" ]
+				then
+					snapState="PowerOn"
+				else
+					snapState="PowerOff"
+				fi
+				printf "| %-25s | %-20s | %-20s |\n" "Date taken" "vCD Snap Size (MB)" "Vapp Status" 
+				echo "|-------------------------------------------------------------------------|"
+				printf "| %-25s | %-20s | %-20s |\n" $snapDate $(($snapSize/1024/1024)) $snapState 
+			else
+				echo "No snapshot found for $vappName."
+				exit 1
+			fi
 		fi
 	;;
 	revert)
