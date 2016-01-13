46109176a2e1faf808e95997d14342e35f9d7a2c
Pierre Garel <pierre@garel.net>
2016-01-13 14:58:22 +0100

    some updates

--- create_vca_profile.sh
+++ create_vca_profile.sh
@@ -8,17 +8,23 @@ then
     exit 10
 fi
 
+# Check availability of needed commands:
+command -v vca >/dev/null 2>&1 || { echo >&2 "I require vca-cli but it's not installed.  Aborting."; exit 1; }
+command -v crudini >/dev/null 2>&1 || { echo >&2 "I require crudini but it's not installed.  Aborting."; exit 1; }
+command -v xml2 >/dev/null 2>&1 || { echo >&2 "I require xml2 but it's not installed.  Aborting."; exit 1; }
+command -v http >/dev/null 2>&1 || { echo >&2 "I require HTTPie but it's not installed.  Aborting."; exit 1; }
+
+
 if [ -f "$HOME/.vcloud-scripts-config" ]
 then
 	libPath=$(crudini --get $HOME/.vcloud-scripts-config Global library_path)
 	vcaBin=$(crudini --get $HOME/.vcloud-scripts-config Global vca_bin) 
 else
 	libPath="./"
-	vcaBin=$(type -p vca)
 fi
 
 CREDFILE="$1"
-[ ! -f $CREDFILE ] && { echo "$INPUT file not found"; exit 99; }
+[ ! -f $CREDFILE ] && { echo "$CREDFILE file not found"; exit 99; }
 
 source $libPath/vcloud-api-func
 source $CREDFILE
